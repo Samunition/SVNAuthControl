e1e2691ca53bf956a83f30c9ae2543653812a294
p-q-lear <jamie@dayan.co.uk>
2017-04-29 17:54:25 +0100

    Not yet working
    but give it time

--- app/scripts/spmFunc.js
+++ app/scripts/spmFunc.js
@@ -709,7 +709,7 @@ function filterGroupsList() {
 	var lGroups = ul.getElementsByTagName("li");
 	var groups = Rules.ruleSet[0]; //Get the groups
 	var activeRepos = getActiveItems("lRepos"); //Get selected repos
-	var activeUsers = getActiveItems("lUsers"); //Get selected repos
+	var activeUsers = getActiveItems("lUsers"); //Get selected users
 	var thisPermission = 0;
 	var perms;
 	
@@ -729,10 +729,8 @@ function filterGroupsList() {
 	if (relevantGroupsOnly) { //If the groups list should be filtered
 		for (var i=0; i<lGroups.length; i++) { //For every group
 			removeReadImage(lGroups[i]);
-			if ((activeRepos.length != 0) && (activeUsers.length != 0)) {
-				lGroups[i].style.display = "none"; //Hide the group in the list
-			}
 			if (activeRepos.length != 0) { //If only groups with access to the selected repositories should appear
+				lGroups[i].style.display = "none"; //Hide the group in the list
 				perms = ruleLoader(groups[i][0]); //Get the group's repositories
 				for (var j=0; j<activeRepos.length; j++) { //For every selected repository
 					for (var k=0; k<perms.length; k++) { //For every repository that the group has permissions for
@@ -745,6 +743,8 @@ function filterGroupsList() {
 				for (var j=0; j<activeUsers.length; j++) { //For every selected user
 					if (thisGroup.indexOf(activeUsers[j].innerText) != -1) { //If it is there
 						lGroups[i].style.display = "block"; //Show the group
+					} else {
+						lGroups[i].style.display = "none"; //Hide the group
 					}
 				}
 			}
@@ -752,6 +752,63 @@ function filterGroupsList() {
 	} else { //If the groups list should not be filtered
 		for (var i=0; i<lGroups.length; i++) {
 			lGroups[i].style.display = 'block'; //Just show everything
+		}
+		document.getElementById("searchGroups").value = ""; //Clear searchbox to avoid confusion
+	}
+}
+
+function filterUsersList() {
+	var relevantUsersOnly = document.getElementById("filterUsers").checked;
+	var ul = document.getElementById("lUsers");
+	var lUsers = ul.getElementsByTagName("li");
+	var groups = Rules.ruleSet[0] //Get the groups
+	var users = Rules.ruleSet[1]; //Get the users
+	var activeRepos = getActiveItems("lRepos"); //Get selected repos
+	var activeUsers = getActiveItems("lUsers"); //Get selected groups
+	var thisPermission = 0;
+	var thisGroupsUsers;
+	var perms;
+	
+	if (activeRepos.length != 0) { //If any repos are selected
+		perms = ruleLoader(users[i][0]); //Get the user's repositories
+		for (var j=0; j<activeRepos.length; j++) { //For every selected repository
+			for (var k=0; k<perms.length; k++) { //For every repository that the user has permissions for
+				if (perms[k][1] == "r") { //If the permission is read-only
+					addReadOnlyImage(lUsers[i]); //Show the "READ" icon alongside the user
+				} else {
+					addReadWriteImage(lUsers[i]); //Show the "WRITE" icon alongside the user
+				}
+			}
+		}
+	}
+			
+	if (relevantUsersOnly) { //If the users list should be filtered
+		for (var i=0; i<lUsers.length; i++) { //For every user
+			removeReadImage(lUsers[i]);
+			if (activeRepos.length != 0) { //If only users with access to the selected repositories should appear
+				lUsers[i].style.display = "none"; //Hide the user in the list
+				perms = ruleLoader(users[i][0]); //Get the user's repositories
+				for (var j=0; j<activeRepos.length; j++) { //For every selected repository
+					for (var k=0; k<perms.length; k++) { //For every repository that the user has permissions for
+						lUsers[i].style.display = "block"; //Show the user
+					}
+				}
+			}
+			if (activeGroups.length != 0) { //If only users in selected groups should appear
+				for (var j=0; j<activeGroups.length; j++) { //For every selected group
+					var inThisGroup = false;
+					thisGroupsUsers = groupUsersLoader(lUsers[i].innerText);
+					if (thisGroupsUsers.indexOf(lUsers[i].innerText) != -1) { //If it is there
+						lUsers[i].style.display = "block"; //Show the user
+					} else {
+						lUsers[i].style.display = "none"; //Hide the user
+					}
+				}
+			}
+		}
+	} else { //If the groups list should not be filtered
+		for (var i=0; i<lUsers.length; i++) {
+			lUsers[i].style.display = 'block'; //Just show everything
 		}
 		document.getElementById("searchGroups").value = ""; //Clear searchbox to avoid confusion
 	}
@@ -776,12 +833,6 @@ function filterReposList() {
 		}
 		document.getElementById("searchRepos").value = ""; //Clear searchbox to avoid confusion
 	}
-}
-
-function filterUsersList() {
-}
-
-function filterReposList() {
 }
 
 function populateGroups() {
